880c1966d1754929f3819047a86bc83598859e2f
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2024-03-27 23:30:17 +0000

    Changed path to generate_executables

--- scripts_nvidia/run_multi_node.sh
+++ scripts_nvidia/run_multi_node.sh
@@ -8,7 +8,7 @@ NUM_RUNS=$2
 EXECUTABLES=("clover_leaf_per_app" "clover_leaf_per_kernel" "clover_leaf_per_phase_hiding" "clover_leaf_per_phase_no_hiding")
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-./generate_executables.sh
+${SCRIPT_DIR}/generate_executables.sh
 
 for ((i=0; i<${NUM_RUNS}; i++)); do
     for ((i=0; i<${#NUM_NODES[@]}; i++)); do
